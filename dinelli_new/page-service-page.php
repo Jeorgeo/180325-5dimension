d78864a5857862e9e79dc178f0fec521159316db
Yury_Shakhouski <Jeorgeo@list.ru>
2018-04-03 17:17:45 +0300

    180403-1717
    preproduction

--- dinelli_new/page-service-page.php
+++ dinelli_new/page-service-page.php
@@ -34,10 +34,11 @@ $news = get_posts(
 
 <main class="content-area">
 	<section class="service box-1">
+		<?php the_post(); ?>
+		<?php the_content(); ?>
+	</section>
+	<section class="service box-2">
 		<div class="container">
-			<h1 class="small-title box">
-				<?php the_field('service-title'); ?>
-			</h1>
 			<div class="box-content">
 				<?php
 					foreach ($news as $obj) {
@@ -46,89 +47,22 @@ $news = get_posts(
 						}
 				 ?>
 				<div class="cols col-3 col-md-6">
-					<a class="service__card" href="/service/<?php echo $obj->post_name; ?>" target="_blank">
-						<img src="<?php echo get_field('events-image',$obj->ID); ?>" alt="<?php echo $obj->post_title; ?>">
-						<p><?php echo get_field('events-description', $obj->ID); ?></p>
+					<a class="service__card" href="<?php echo get_field('service-link', $obj->ID); ?>" target="_blank">
+						<figure>
+							<img src="<?php echo get_field('service-image',$obj->ID); ?>" alt="<?php echo $obj->post_title; ?>">
+						</figure>
+						<p>
+							<?php echo get_field('service-description', $obj->ID); ?>
+							<span class="service__price"><?php echo get_field('service-price', $obj->ID); ?></span>
+						</p>
 					</a>
-					<a class="service__btn" href="<?php echo get_field('events-link', $obj->ID); ?>">Подробнее...</a>
+					<a class="service__btn" href="<?php echo get_field('service-link', $obj->ID); ?>">Подробнее...</a>
 				</div>
 
 				<?php
 					}
 				 ?>
 
-			</div>
-		</div>
-	</section>
-	<section class="service box-2">
-		<div class="container">
-			<span class="box-title">
-				аромат прибыли
-			</span>
-			<div class="box-content clearfix">
-				<h4 class="small-title box">
-					Пришла пора...
-				</h4>
-				<div class="row clearfix">
-					<div class="cols col-6">
-						<?php the_field('pole3'); ?>
-					</div>
-				</div>
-				<div class="row clearfix">
-					<div class="cols col-3 col-md-6 left_side">
-						<a class="service__card" href="<?php echo get_field('cons'); ?>" target="_blank">
-							<img src="<?php echo get_field('cons_img'); ?>">
-						</a>
-					</div>
-					<div class="cols col-3 col-md-6">
-						<a class="service__card" href="<?php echo get_field('audit'); ?>" target="_blank">
-							<img src="<?php echo get_field('audit_img'); ?>">
-						</a>
-					</div>
-					<div class="cols col-3 col-md-6 right_side">
-						<a class="service__card" href="<?php echo get_field('coach'); ?>" target="_blank">
-							<img src="<?php echo get_field('coach_img'); ?>">
-						</a>
-					</div>
-				</div>
-				<div class="row clearfix">
-					<div class="cols col-5">
-						<?php the_field('pole4'); ?>
-					</div>
-					<div class="cols col-5 bottom-form">
-						<form id="js_form" class="cloud-form order-form" method="post">
-							<!-- Hidden Required Fields -->
-							<input type="hidden" name="project_name" value="Dinelli.ru">
-							<?php dynamic_sidebar( 'admin_mail' ); ?>
-							<input type="hidden" name="form_subject" value="Заявка с сайта. Все услуги">
-							<!-- END Hidden Required Fields -->
-							<label class="cloud-form__phone" for="phone">Телефон</label>
-							<input id="phone" class="cloud-form__phone" type="text" name="phone"
-							value="" placeholder="+7(___) ___-__-__" required
-							pattern="^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$">
-							<label class="cloud-form__mail" for="mail">e-mail</label>
-							<input id="mail"  class="cloud-form__mail" type="text" name="mail"
-							value="" placeholder="iNfo@DiNelli.ru" required
-							pattern="^([A-Za-z0-9_\.\-]{1,20})@([a-z0-9\.\-]{1,20})\.([a-z]{2,4})">
-
-							<button id="submit" class="cloud-form__submit" type="submit" name="submit" onclick="yaCounter39506875.reachGoal('ORDER13'); return true;">
-								Отправить заявку
-							</button>
-							<input id="сonsent" class="cloud-form__сonsent" type="checkbox" name="сonsent" value="" required>
-							<label class="cloud-form__сonsent" for="сonsent">
-								Согласие на обработку персональных данных
-							</label>
-						</form>
-					</div>
-				</div>
-				<div class="row clearfix">
-					<div class="cols col-5 col-md-6">
-						<img class="content-img full" src="<?php the_field('img-o'); ?>" alt="">
-					</div>
-					<div class="cols col-6 col-md-6 umbrella">
-						<?php the_field('pole14'); ?>
-					</div>
-				</div>
 			</div>
 		</div>
 	</section>
